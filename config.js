6bd41ea6cb8211edba9b2f3141730192c05758a1
Paulo Canedo <paulocanedo@gmail.com>
2018-02-27 20:39:57 -0300

    more popular values

--- config.js
+++ config.js
@@ -1,16 +1,8 @@
 module.exports = {
-  host: 'ws://192.168.0.100:3000',
+  host: 'ws://localhost:3000',
   outputfile: './history.json',
   topics: [
-    'casa/+/temperatura',
-    'casa/+/humidade'
+    'home/+/temperature',
+    'home/+/humidity'
   ]
 };
-// module.exports = {
-//   host: 'ws://localhost:3000',
-//   outputfile: './history.json',
-//   topics: [
-//     'home/+/temperature',
-//     'home/+/humidity'
-//   ]
-// };
